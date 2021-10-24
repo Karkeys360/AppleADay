21c237b5fcbfbf06e479bcbb171659d64e1a8dcb
Alan5032 <46549617+Alan5032@users.noreply.github.com>
2021-10-24 06:09:35 -0700

    removed mail icon and toolbar

--- app/src/main/java/com/dh21/appleaday/AddFoodItemsActivity.java
+++ app/src/main/java/com/dh21/appleaday/AddFoodItemsActivity.java
@@ -32,18 +32,7 @@ public class AddFoodItemsActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_add_food_items);
-        Toolbar toolbar = findViewById(R.id.toolbar);
-        setSupportActionBar(toolbar);
         openScanner = findViewById(R.id.openScanner);
-
-        FloatingActionButton fab = findViewById(R.id.fab);
-        fab.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View view) {
-                Snackbar.make(view, "Replace with your own action", Snackbar.LENGTH_LONG)
-                        .setAction("Action", null).show();
-            }
-        });
 
         ActivityResultLauncher<Void> launcher = registerForActivityResult(new ScannerContract(), food -> {
             if (food != null) {
