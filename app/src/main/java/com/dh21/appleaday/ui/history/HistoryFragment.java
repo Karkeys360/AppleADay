010ce8a1f6e681e9e0acf89130e8ff5fb9f16b47
Abhay D <a.deshpande012@gmail.com>
2021-10-24 06:04:00 -0700

    Change color of buttons in history

--- app/src/main/java/com/dh21/appleaday/ui/history/HistoryFragment.java
+++ app/src/main/java/com/dh21/appleaday/ui/history/HistoryFragment.java
@@ -4,20 +4,26 @@ import android.os.Bundle;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
-import android.widget.TextView;
 
 import androidx.annotation.NonNull;
-import androidx.annotation.Nullable;
 import androidx.fragment.app.Fragment;
-import androidx.lifecycle.Observer;
 import androidx.lifecycle.ViewModelProvider;
 
+import com.dh21.appleaday.R;
 import com.dh21.appleaday.databinding.FragmentHistoryBinding;
 
 public class HistoryFragment extends Fragment {
 
+    private static int DEF_BTN_RES = R.drawable.roundedbutton;
+    private static int PRESSED_BTN_RES = R.drawable.roundedbutton_pressed;
+
+    private enum Display {
+        Food, Event
+    }
+
     private HistoryViewModel historyViewModel;
     private FragmentHistoryBinding binding;
+    private Display display = Display.Food;
 
     public View onCreateView(@NonNull LayoutInflater inflater,
                              ViewGroup container, Bundle savedInstanceState) {
@@ -26,7 +32,36 @@ public class HistoryFragment extends Fragment {
 
         binding = FragmentHistoryBinding.inflate(inflater, container, false);
         View root = binding.getRoot();
+
+        binding.eventsButton.setOnClickListener(this::eventsButtonClicked);
+        binding.mealsButton.setOnClickListener(this::foodsButtonClicked);
         return root;
+    }
+
+    private void eventsButtonClicked(View v) {
+        this.display = Display.Event;
+        populateData();
+    }
+
+    private void foodsButtonClicked(View v) {
+        this.display = Display.Food;
+        populateData();
+    }
+
+    @Override
+    public void onStart() {
+        super.onStart();
+        populateData();
+    }
+
+    private void populateData() {
+        if (display == Display.Event) {
+            binding.mealsButton.setBackgroundResource(DEF_BTN_RES);
+            binding.eventsButton.setBackgroundResource(PRESSED_BTN_RES);
+        } else {
+            binding.mealsButton.setBackgroundResource(PRESSED_BTN_RES);
+            binding.eventsButton.setBackgroundResource(DEF_BTN_RES);
+        }
     }
 
     @Override
