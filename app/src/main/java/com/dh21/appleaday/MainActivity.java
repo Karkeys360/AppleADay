dd44f18d35c90389d446f5aeabf16dfa916e88c3
Alan5032 <46549617+Alan5032@users.noreply.github.com>
2021-10-23 18:38:51 -0700

    implemented save to disk and get from disk

--- app/src/main/java/com/dh21/appleaday/MainActivity.java
+++ app/src/main/java/com/dh21/appleaday/MainActivity.java
@@ -1,14 +1,37 @@
 package com.dh21.appleaday;
 
+import static com.dh21.appleaday.analysis.AnalysisUtil.*;
+
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.util.Log;
+import android.widget.TextView;
+
+import java.util.HashMap;
+import java.util.Map;
 
 public class MainActivity extends AppCompatActivity {
+
+    private TextView text;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        text = findViewById(R.id.text);
+
+        StringBuilder sb = new StringBuilder();
+        String string1 = "put this to disk!";
+        sb.append(string1 + "\n");
+        saveToDisk(this, string1);
+        sb.append("-------\n");
+
+
+        String string2 = "test";
+        sb.append(string2 + "\n");
+        string2 = getFromDisk(this) +  "test2";
+        sb.append(string2);
+        text.setText(sb.toString());
     }
 }
--- app/src/main/java/com/dh21/appleaday/analysis/AnalysisUtil.java
+++ app/src/main/java/com/dh21/appleaday/analysis/AnalysisUtil.java
@@ -3,24 +3,44 @@ package com.dh21.appleaday.analysis;
 import android.content.Context;
 
 import java.io.File;
-import java.io.FileWriter;
+import java.io.FileOutputStream;
 import java.util.HashMap;
 import java.util.Map;
+import java.util.Scanner;
+
+import com.google.gson.Gson;
+
 
 public class AnalysisUtil {
 
-    public void saveToDisk(Context context, Map<String, Double> map) {
-
-        Gson gson = new Gson();
-        gson.toJson(map, new FileWriter(context.getFilesDir() + "map.json"));
-
-        File dir = new File(context.getFilesDir(), "analytics");
+    public static void saveToDisk(Context context, String s) {
+        File oldFile = new File(context.getFilesDir() + "/string");
+        if (oldFile.exists()) {
+            oldFile.delete();
+        }
+        File file = new File(context.getFilesDir(), "string");
+        try (FileOutputStream outputStream = new FileOutputStream(file)){
+            outputStream.write(s.getBytes());
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
     }
 
-    public Map<String, Double> getFromDisk() {
-        Map<String, Double> map = new HashMap<>();
-        return map;
+    public static String getFromDisk(Context context) {
+        String s = "";
+        File file = new File(context.getFilesDir() + "/string");
+        if (file.exists()) {
+            try (Scanner scanner = new Scanner(context.openFileInput("string")).useDelimiter("\\Z")) {
+                StringBuilder sb = new StringBuilder();
+                while (scanner.hasNext()) {
+                    sb.append(scanner.next());
+                }
+                s = sb.toString();
+            } catch (Exception e) {
+                e.printStackTrace();
+            }
+        }
+        return s;
     }
-
 }
 
