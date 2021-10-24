5c7fe55b0780fafe7a5accecaf158835114c55f1
Abhay D <a.deshpande012@gmail.com>
2021-10-24 06:57:57 -0700

    Got columns to line up appropriately

--- app/src/main/java/com/dh21/appleaday/ui/history/HistoryFragment.java
+++ app/src/main/java/com/dh21/appleaday/ui/history/HistoryFragment.java
@@ -1,21 +1,38 @@
 package com.dh21.appleaday.ui.history;
 
 import android.os.Bundle;
+import android.view.Gravity;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.LinearLayout;
+import android.widget.TextView;
 
 import androidx.annotation.NonNull;
 import androidx.fragment.app.Fragment;
+import androidx.gridlayout.widget.GridLayout;
 import androidx.lifecycle.ViewModelProvider;
 
 import com.dh21.appleaday.R;
+import com.dh21.appleaday.analysis.EventAnalysis;
+import com.dh21.appleaday.data.Event;
+import com.dh21.appleaday.data.Food;
+import com.dh21.appleaday.data.Timed;
 import com.dh21.appleaday.databinding.FragmentHistoryBinding;
+
+import java.text.SimpleDateFormat;
+import java.util.Arrays;
+import java.util.Date;
+import java.util.List;
+import java.util.Locale;
+import java.util.stream.Collectors;
 
 public class HistoryFragment extends Fragment {
 
     private static final int DEF_BTN_RES = R.drawable.roundedbutton;
     private static final int PRESSED_BTN_RES = R.drawable.roundedbutton_pressed;
+
+    private static final int NUM_DISPLAYED = 50;
 
     private enum Display {
         Food, Event
@@ -62,6 +79,66 @@ public class HistoryFragment extends Fragment {
             binding.mealsButton.setBackgroundResource(PRESSED_BTN_RES);
             binding.eventsButton.setBackgroundResource(DEF_BTN_RES);
         }
+        binding.contentGrid.removeAllViews();
+
+        EventAnalysis ea = EventAnalysis.getInstance();
+        List<Timed> timeds = ea.getTimes();
+        int added = 0;
+        for (int i = timeds.size() - 1; i >= 0 && added < NUM_DISPLAYED; i--) {
+            Timed t = timeds.get(i);
+            if (display == Display.Event && t instanceof Event) {
+                Event e = (Event) t;
+                addRowToLayout(binding.contentGrid, e.getName(), e.getTime(), added);
+                added++;
+            } else if (display == Display.Food && t instanceof Food) {
+                Food f = (Food) t;
+                addRowToLayout(binding.contentGrid, f.getName(), f.getTime(), added);
+                added++;
+            }
+        }
+    }
+
+    private String titleCase(String str) {
+        String[] words = str.split("\\s");
+        return Arrays.stream(words)
+                .map(s -> s.substring(0, 1).toUpperCase() + s.substring(1).toLowerCase())
+                .collect(Collectors.joining(" "));
+    }
+
+    private int dpToPixels(int dp) {
+        final float scale = requireContext().getResources().getDisplayMetrics().density;
+        return (int) (dp * scale + 0.5f);
+    }
+
+    private void addRowToLayout(GridLayout gl, String name, long timestamp, int row) {
+        TextView nameText = new TextView(requireContext());
+        nameText.setText(name);
+        nameText.setGravity(Gravity.CENTER);
+
+        SimpleDateFormat sdf = new SimpleDateFormat("MM/dd", Locale.US);
+        String dateStr = sdf.format(new Date(timestamp));
+        TextView dateText = new TextView(requireContext());
+        dateText.setGravity(Gravity.CENTER);
+        dateText.setText(dateStr);
+
+        gl.addView(nameText);
+        gl.addView(dateText);
+
+        nameText.setLayoutParams(createGLParam(row, 0));
+        dateText.setLayoutParams(createGLParam(row, 1));
+    }
+
+    private GridLayout.LayoutParams createGLParam(int r, int c) {
+        GridLayout.LayoutParams param =new GridLayout.LayoutParams();
+        param.height = GridLayout.LayoutParams.WRAP_CONTENT;
+        param.width = GridLayout.LayoutParams.WRAP_CONTENT;
+        param.setMargins(0, dpToPixels(3), 0, dpToPixels(3));
+//        param.rightMargin = 5;
+//        param.topMargin = 5;
+//        param.setGravity(Gravity.CENTER);
+        param.columnSpec = GridLayout.spec(c, 1f);
+        param.rowSpec = GridLayout.spec(r);
+        return param;
     }
 
     @Override
