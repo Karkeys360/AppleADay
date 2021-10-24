0b91417c448602cca89b3aacb1e2df97d3eed98d
Alan5032 <46549617+Alan5032@users.noreply.github.com>
2021-10-23 22:29:02 -0700

    fixed IntervalIterator 3

--- app/src/main/java/com/dh21/appleaday/data/IntervalIterator.java
+++ app/src/main/java/com/dh21/appleaday/data/IntervalIterator.java
@@ -33,31 +33,47 @@ public class IntervalIterator {
 
         long startMilli = endMilli;
         Calendar startDate = (Calendar) endDate.clone();
+        int year = 0;
 
         switch (interval) {
             case Day:
                 int day = endDate.get(Calendar.DAY_OF_YEAR);
-                while (startDate.get(Calendar.DAY_OF_YEAR) == day && startIndex != -1) {
+                year = endDate.get(Calendar.YEAR);
+                while (startDate.get(Calendar.DAY_OF_YEAR) == day &&
+                        startDate.get(Calendar.YEAR) == year && startIndex != 0) {
+                    startIndex -= 1;
                     startMilli = times.get(startIndex).getTime();
                     startDate.setTimeInMillis(startMilli);
+                }
+                if (startIndex == 0 && startDate.get(Calendar.DAY_OF_YEAR) == day) {
                     startIndex -= 1;
                 }
                 break;
 
             case Week:
-                int week = endDate.get(Calendar.WEEK_OF_MONTH);
-                while (startDate.get(Calendar.WEEK_OF_MONTH) == week && startIndex != -1) {
+                int week = endDate.get(Calendar.WEEK_OF_YEAR);
+                year = endDate.get(Calendar.YEAR);
+                while (startDate.get(Calendar.WEEK_OF_YEAR) == week &&
+                        startDate.get(Calendar.YEAR) == year && startIndex != 0) {
+                    startIndex -= 1;
                     startMilli = times.get(startIndex).getTime();
                     startDate.setTimeInMillis(startMilli);
+                }
+                if (startIndex == 0 && startDate.get(Calendar.WEEK_OF_YEAR) == week) {
                     startIndex -= 1;
                 }
                 break;
 
             case Month:
                 int month = endDate.get(Calendar.MONTH);
-                while (startDate.get(Calendar.MONTH) == month && startIndex != -1) {
+                year = endDate.get(Calendar.YEAR);
+                while (startDate.get(Calendar.MONTH) == month &&
+                        startDate.get(Calendar.YEAR) == year && startIndex != 0) {
+                    startIndex -= 1;
                     startMilli = times.get(startIndex).getTime();
                     startDate.setTimeInMillis(startMilli);
+                }
+                if (startIndex == 0 && startDate.get(Calendar.MONTH) == month) {
                     startIndex -= 1;
                 }
                 break;
