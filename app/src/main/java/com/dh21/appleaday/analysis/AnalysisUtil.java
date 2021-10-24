f426ce031c27af5677637a6effc2b879c5cc82f8
Alan5032 <46549617+Alan5032@users.noreply.github.com>
2021-10-23 18:48:07 -0700

    fixed saveToDisk

--- app/src/main/java/com/dh21/appleaday/analysis/AnalysisUtil.java
+++ app/src/main/java/com/dh21/appleaday/analysis/AnalysisUtil.java
@@ -4,6 +4,7 @@ import android.content.Context;
 
 import java.io.File;
 import java.io.FileOutputStream;
+import java.io.IOException;
 import java.util.Scanner;
 
 public class AnalysisUtil {
@@ -16,13 +17,14 @@ public class AnalysisUtil {
         File file = new File(context.getFilesDir(), "string");
         try (FileOutputStream outputStream = new FileOutputStream(file)){
             outputStream.write(s.getBytes());
-        } catch (Exception e) {
+        } catch (IOException e) {
             e.printStackTrace();
+            throw new RuntimeException(e);
         }
     }
 
     public static String getFromDisk(Context context) {
-        String s = "";
+        String s = null;
         File file = new File(context.getFilesDir() + "/string");
         if (file.exists()) {
             try (Scanner scanner = new Scanner(context.openFileInput("string")).useDelimiter("\\Z")) {
@@ -31,8 +33,9 @@ public class AnalysisUtil {
                     sb.append(scanner.next());
                 }
                 s = sb.toString();
-            } catch (Exception e) {
+            } catch (IOException e) {
                 e.printStackTrace();
+                throw new RuntimeException(e);
             }
         }
         return s;
