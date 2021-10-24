0641569d07819c6b9f5a3abea061e9d0e52c65fe
Alan5032 <46549617+Alan5032@users.noreply.github.com>
2021-10-24 05:15:26 -0700

    implemented FoodEditorActivity

---
+++ app/src/main/java/com/dh21/appleaday/FoodEditorActivity.java
@@ -0,0 +1,14 @@
+package com.dh21.appleaday;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class FoodEditorActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_food_editor);
+    }
+}
