f6d3ffbba09b2891139a1274fb0cef75dd76a57c
Alan5032 <46549617+Alan5032@users.noreply.github.com>
2021-10-23 18:52:43 -0700

    reverted MainActivity

--- app/src/main/java/com/dh21/appleaday/MainActivity.java
+++ app/src/main/java/com/dh21/appleaday/MainActivity.java
@@ -1,24 +1,14 @@
 package com.dh21.appleaday;
 
-import static com.dh21.appleaday.analysis.AnalysisUtil.*;
-
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
-import android.util.Log;
-import android.widget.TextView;
-
-import java.util.HashMap;
-import java.util.Map;
 
 public class MainActivity extends AppCompatActivity {
-
-    private TextView text;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-        text = findViewById(R.id.text);
     }
 }
